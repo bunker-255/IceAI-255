d056510b4f691beceaca2e2777235845df2e663a
bunker-255 <bunker255il@gmail.com>
2025-08-31 02:09:51 +0300

    Add files via upload

---
+++ src/tools.py
@@ -0,0 +1,16 @@
+import pickle
+import os
+
+def pick(filename, data):
+    # Если filename не содержит пути, добавляем директорию conversation
+    if not os.path.dirname(filename):
+        filename = os.path.join("conversation", filename)
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f)
+
+def unpick(filename):
+    # Если filename не содержит пути, добавляем директорию conversation
+    if not os.path.dirname(filename):
+        filename = os.path.join("conversation", filename)
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
